abfe36bb51e14577a91d128e34e288dcb78622ca
Cao Quoc An <155868386+anpc33@users.noreply.github.com>
2025-05-22 12:30:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
     build: {
         manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-        },
+        outDir: 'public/build', // Laravel sẽ tìm manifest ở đây
+        emptyOutDir: true,
     },
 });
